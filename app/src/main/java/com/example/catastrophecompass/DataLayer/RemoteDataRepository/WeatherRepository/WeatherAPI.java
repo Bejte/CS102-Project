aa1cb38bd5f1464bf6c9dc5d98170d3289b060d5
emirtom <emirtomrukcu1@gmail.com>
2023-05-25 12:16:12 +0300

    updated WeatherAPI

--- app/src/main/java/com/example/catastrophecompass/DataLayer/RemoteDataRepository/WeatherRepository/WeatherAPI.java
+++ app/src/main/java/com/example/catastrophecompass/DataLayer/RemoteDataRepository/WeatherRepository/WeatherAPI.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
 
 import com.example.catastrophecompass.DataLayer.Model.WorkplaceWeather;
 import com.google.gson.Gson;
@@ -45,7 +47,8 @@ public class WeatherAPI {
                 Gson gson = new Gson();
                 WeatherData weatherData = gson.fromJson(response.toString(), WeatherData.class);
                 // Access the desired properties
-                // TODO convert WeatherData to WorkplaceWeather
+                // TODO test weatherDataToWorkplaceWeather
+                return weatherDataToWorkplaceWeather(weatherData);
 
             } else {
                 Log.d("WeatherAPI", "API call failed with response code: " + responseCode);
@@ -55,61 +58,23 @@ public class WeatherAPI {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-    public static void main(String[] args) {
-        // Replace {lat}, {lon}, and {lang} with actual values
-        String lat = "39.8746188";
-        String lon = "32.7450159";
-
-        String apiUrlWithParams = apiUrl.replace("{lat}", lat).replace("{lon}", lon);
-        apiUrlWithParams += "&appid=" + apiKey;
-
-        try {
-            URL url = new URL(apiUrlWithParams);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                StringBuilder response = new StringBuilder();
-
-                while ((line = reader.readLine()) != null) {
-                    response.append(line + "\n");
-                }
-                reader.close();
-
-
-                Gson gson = new Gson();
-                WeatherData weatherData = gson.fromJson(response.toString(), WeatherData.class);
-                // Access the desired properties
-                System.out.println("Latitude: " + weatherData.lat);
-                System.out.println("Longitude: " + weatherData.lon);
-                System.out.println("Timezone: " + weatherData.timezone);
-
-                WeatherData.CurrentWeather currentWeather = weatherData.current;
-                System.out.println("Temperature: " + currentWeather.temp);
-                System.out.println("Feels Like: " + currentWeather.feels_like);
-
-                WeatherData.HourlyWeather[] hourlyWeather = weatherData.hourly;
-                System.out.println("Hourly Weather:");
-                for (WeatherData.HourlyWeather hour : hourlyWeather) {
-                    System.out.println("Time: " + hour.dt);
-                    System.out.println("Temperature: " + hour.temp);
-                    System.out.println("Feels Like: " + hour.feels_like);
-                    System.out.println("Description: " + hour.weather[0].description);
-                    System.out.println("Icon: " + hour.weather[0].icon);
-                    System.out.println();
-                }
-            } else {
-                System.out.println("API call failed with response code: " + responseCode);
-            }
-
-            connection.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static WorkplaceWeather weatherDataToWorkplaceWeather(WeatherData data){
+        HashMap<String, String> hourlyTemperature = new HashMap<>();
+        WeatherData.HourlyWeather[] hourlyWeathers = data.hourly;
+        String currentTemp = "" + Math.round(hourlyWeathers[0].temp);
+        String currentWeatherType = hourlyWeathers[0].weather[0].description;
+        currentWeatherType = currentWeatherType.substring(0,1).toUpperCase() + currentWeatherType.substring(1); // TODO uppercase all words
+        for (int i = 1; i < 13; i++)
+        {
+            WeatherData.HourlyWeather hourly = hourlyWeathers[i];
+            Date date = new Date(hourly.dt*1000);
+            String hour = date.getHours() + ".00";
+            String temp = Math.round(hourly.temp) + "°C";
+            hourlyTemperature.put(hour, temp);
         }
+        return new WorkplaceWeather(currentTemp, currentWeatherType, hourlyTemperature);
     }
 }
