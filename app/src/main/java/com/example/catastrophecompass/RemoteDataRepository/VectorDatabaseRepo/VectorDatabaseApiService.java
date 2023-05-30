895b808580720160023d7adc3763f7506901ced7
DSNGORKEM <gorkemaslandsn@gmail.com>
2023-05-30 13:36:26 +0300

    GORKEM: Vector DB Rest api started

--- app/src/main/java/com/example/catastrophecompass/RemoteDataRepository/VectorDatabaseRepo/VectorDatabaseApiService.java
+++ app/src/main/java/com/example/catastrophecompass/RemoteDataRepository/VectorDatabaseRepo/VectorDatabaseApiService.java
@@ -1,4 +1,22 @@
 package com.example.catastrophecompass.RemoteDataRepository.VectorDatabaseRepo;
 
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 public class VectorDatabaseApiService {
+
+    /**
+     * Rest Api for Pinecone
+     */
+    public interface PineconeApiService {
+        @Headers({
+                "Api-Key: 0d6b6c99-ea18-4725-b036-a503901925cb",
+                "Accept: application/json",
+                "Content-Type: application/json"
+        })
+        @POST("vectors/upsert")
+        Single<YourResponseModel> upsertVectors(@Body YourRequestModel request);
+
+    }
 }
