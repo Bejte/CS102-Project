dfc625c905fe3403f334230f0b814cb4443f1589
emirtom <emirtomrukcu1@gmail.com>
2023-05-31 18:01:27 +0300

    updated HQAddEditMemberVM

--- app/src/main/java/com/example/catastrophecompass/UILayer/HQOrganizer/Admin/HQAddEditMemberVM.java
+++ app/src/main/java/com/example/catastrophecompass/UILayer/HQOrganizer/Admin/HQAddEditMemberVM.java
@@ -5,10 +5,17 @@ import android.util.Log;
 import androidx.lifecycle.ViewModel;
 
 import com.example.catastrophecompass.DataLayer.FBRepository.HQAddEditMemberFBRepo;
+import com.example.catastrophecompass.DataLayer.Model.HQO;
+import com.example.catastrophecompass.DataLayer.Model.LogisticInfo;
 import com.example.catastrophecompass.DataLayer.Model.OrganizationAsNode;
 import com.example.catastrophecompass.DataLayer.Model.TeamOrganizator;
+import com.example.catastrophecompass.DataLayer.Model.UserLogin;
 
 import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
 
 @HiltViewModel
 public class HQAddEditMemberVM extends ViewModel {
@@ -21,18 +28,18 @@ public class HQAddEditMemberVM extends ViewModel {
 
 
 
-    public void addLogisticMember(Logistics logistics, String organizationName){
+    public void addLogisticMember(LogisticInfo logistics, String organizationName, UserLogin userLogin){ // TODO Add this to View
         Log.d("HQAddEditMemberVM", "addLogisticMember() called");
-        return FBRepo.addLogisticMember(logistics,organizationName;
+        return FBRepo.addLogisticMember(logistics,organizationName, userLogin);
     }
 
     public void addHQOrganizator(HQO hqo, String organizationName){
         Log.d("HQAddEditMemberVM", "addLogisticMember() called");
-        return FBRepo.addHQOrganizator(hqo,organizationName;
+        return FBRepo.addHQOrganizator(hqo,organizationName);
     }
 
     public void addTeamOrganizator(TeamOrganizator to, String organizationName){
         Log.d("HQAddEditMemberVM", "addLogisticMember() called");
-        return FBRepo.addTeamOrganizator(to,organizationName;
+        return FBRepo.addTeamOrganizator(to,organizationName);
     }
 }
